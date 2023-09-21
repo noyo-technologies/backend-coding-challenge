9b26e7b208a4b2e3dd83364f9e6db2fa8904ca0b
Dave Gregory <screwlewse@gmail.com>
2023-09-20 22:36:09 -0700

    final work done

--- service/api/segments.py
+++ service/api/segments.py
@@ -1,18 +1,19 @@
 from datetime import datetime
 from uuid import UUID
 
-from flask import abort, jsonify
+from flask import abort, jsonify, request
 from webargs.flaskparser import use_args
 
 from marshmallow import Schema, fields
 
-from service.server import app
-from service.models import Person
+from service.server import app, db
+from service.models import Person, Segment
 
 
 # Query Schemas
 class GetSegmentQueryArgsSchema(Schema):
     date = fields.Date(required=False, missing=datetime.utcnow().date())
+    limit = fields.Bool(required=False, missing=False)
 
 
 # Validation and Response Schemas
@@ -28,31 +29,163 @@ class SegmentSchema(Schema):
     end_date = fields.Date(required=False)
 
 
-@app.route("/api/persons/<uuid:person_id>/segment", methods=["GET"])
-@use_args(GetSegmentQueryArgsSchema(), location="querystring")
-def get_segment(args: dict, person_id: UUID):
-    person = Person.query.get(person_id)
+# Response Schemas
+class SegmentResponseSchema(Schema):
+    class Meta:
+        ordered = True
 
+    id = fields.UUID(required=True)
+    person_id = fields.UUID(required=True)
+    city = fields.Str(required=True, max=128)
+    state = fields.Str(required=True, max=2)
+    zip_code = fields.Str(required=True, max=10)
+
+    start_date = fields.Date(required=True)
+    end_date = fields.Date(required=False)
+
+
+@app.route("/api/persons/<uuid:person_id>/segments", methods=["GET"])
+@use_args(GetSegmentQueryArgsSchema(), location="querystring")
+def get_segments(args: dict, person_id: UUID):
+    """Get segment(s) for person.
+
+    Args:
+    ----
+        args (dict): optional arguments for refinement of the query
+            date: use a date other than today
+            limit: (BOOL) whether to show all segments for that date, or just one
+        person_id (UUID): unique id of the person, stored in the database.
+
+    Returns:
+    -------
+        _type_: _description_
+
+    Notes:
+    -----
+    This API should use the plural of segment instead.
+    This would be more consistent with the REST rules and existing URI's.
+
+    I'm going to make the change based on the assumption that this API
+    is not yet being used. However, if this API is already being used
+    by clients, we dont want to change it on them. Instead, we could
+    create a strategy to change it in future versions and include
+    deprecation messages in the existing API, and ensure that the
+    breaking change is communicated in the notes of future versions of the API
+    """
+    person = get_person_by_id(person_id)
     if person is None:
         abort(404, description="person does not exist")
     elif len(person.segments) == 0:
-        abort(404, description="person does not have an segment, please create one")
+        abort(404, description="person does not have a segment, please create one")
 
     # query date comes in the the args and default to "today"
     query_date = args["date"]
 
-    # Just get the the first segment that comes back
-    segment = person.segments[0]
-    return jsonify(SegmentSchema().dump(segment))
+    limit = args["limit"]
+
+    if limit:
+        # if the limit is true, only send the last item in a sequence
+        # for future, using "range" could allow for number
+        # of items to retrieve/show
+        segments = [person.segments[-1]]
+    else:
+        # otherwise default to all segments
+        segments = person.segments
+    return jsonify(SegmentResponseSchema(many=True).dump(segments))
 
 
-@app.route("/api/persons/<uuid:person_id>/merge", methods=["PUT"])
+@app.route("/api/persons/<uuid:person_id>/segments", methods=["PUT"])
 @use_args(SegmentSchema())
-def merge_segments(payload: dict, person_id: UUID):
-    return "Not Implemented", 501
+def upsert_segment(payload: dict, person_id: UUID):
+    """Insert or update segment based on:
+        * if no segments for the person. CREATE NEW
+        * if new segment start date is:
+            * same as latest segment start date: UPDATE EXISTING
+            * earlier than latest segment end date: FAIL
+            * later than latest segment start date, and end date is null:
+                UPDATE EXISTING, & CREATE NEW
+            * later than latest segment end date: CREATE NEW
+
+    Args:
+        payload (dict): _description_
+        person_id (UUID): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    person = get_person_by_id(person_id)
+    if person is None:
+        abort(404, description="No person found for segment")
+
+    new_segment_data = fill_segment(payload, person_id)
+
+    if len(person.segments) == 0:
+        # if there are no segments for this person,
+        # then create the first one
+        new_segment = create_segment(new_segment_data)
+        return jsonify(SegmentResponseSchema().dump(new_segment))
+
+    # otherwise get the latest segment
+    previous_segment = person.segments[-1]
+
+    if new_segment_data.start_date == previous_segment.start_date:
+        updated_segment = update_segment(new_segment_data, previous_segment.id)
+        return jsonify(SegmentResponseSchema().dump(updated_segment))
+
+    if new_segment_data.start_date < previous_segment.start_date:
+        # if new segment's start date is before the previous_segment's start date
+        abort(422, description="new segment starts before previous segment ends")
+
+    if not previous_segment.end_date:
+        # update the latest segment with an end date
+        # which will be the same as the new segment start date
+        previous_segment.end_date = new_segment_data.start_date
+        updated_segment = update_segment(previous_segment, previous_segment.id)
+
+        # and create new segment
+        new_segment = create_segment(new_segment_data)
+        return jsonify(SegmentResponseSchema().dump(new_segment))
+    elif new_segment_data.start_date < previous_segment.end_date:
+        # if new segment's start date is before the previous_segment's end date
+        abort(422, description="new segment starts before previous segment ends")
+    else:
+        new_segment = create_segment(new_segment_data)
+        return jsonify(SegmentResponseSchema().dump(new_segment))
 
 
-@app.route("/api/persons/<uuid:person_id>/segment", methods=["PUT"])
-@use_args(SegmentSchema())
-def create_segment(payload: dict, person_id: UUID):
-    return "Not Implemented", 501
+    # this should never trigger
+    app.logger.warn(f"Failed to upsert segment for person: {person_id}")
+
+
+def update_segment(segment, id):
+    """Connect to DB and update existing segment for person"""
+    segment_to_update = db.session.query(Segment).get(id)
+
+    db.session.add(segment_to_update)
+    db.session.commit()
+    db.session.refresh(segment_to_update)
+    return segment_to_update
+
+
+def create_segment(segment: Segment):
+    """Connect to DB and create new segment for person"""
+    db.session.add(segment)
+    db.session.commit()
+    db.session.refresh(segment)
+    return segment
+
+
+def fill_segment(payload: dict, person_id: UUID):
+    """Create new segment based on Segment model"""
+    return Segment(
+        person_id=person_id,
+        city=payload.get("city"),
+        state=payload.get("state"),
+        zip_code=payload.get("zip_code"),
+        start_date=payload.get("start_date"),
+        end_date=payload.get("end_date"),
+    )
+
+
+def get_person_by_id(person_id):
+    return Person.query.get(person_id)
--- tests/api/test_segments.py
+++ tests/api/test_segments.py
@@ -40,11 +40,23 @@ def seed_person_and_segment(seed_person: Person):
     yield segment_segment
 
 
+@pytest.fixture
+def segment_data():
+    return {
+        "city": "San Francisco",
+        "state": "CA",
+        "zip_code": "94613",
+        "start_date": "2021-01-01",
+    }
+
+
 def test_create_segment_with_validation_error(
     test_context: AppContext, client: FlaskClient, seed_person: Person
 ):
     with test_context:
-        response = client.put(f"/api/persons/{seed_person.id}/segment", json={})
+        response = client.put(
+            f"/api/persons/{seed_person.id}/segments?limit=true", json={}
+        )
 
         assert response.status_code == 422
         assert response.json == {
@@ -64,14 +76,14 @@ def test_get_segment(
 ):
     with test_context:
         response = client.get(
-            f"/api/persons/{seed_person_and_segment.person_id}/segment"
+            f"/api/persons/{seed_person_and_segment.person_id}/segments?limit=true"
         )
         assert response.status_code == 200
 
 
 def test_get_segment_no_person(test_context: AppContext, client: FlaskClient):
     with test_context:
-        response = client.get(f"/api/persons/{uuid.uuid4()}/segment")
+        response = client.get(f"/api/persons/{uuid.uuid4()}/segments?limit=true")
         assert response.status_code == 404
         assert response.json == {"error": "person does not exist"}
 
@@ -80,8 +92,204 @@ def test_get_segment_no_segment_exists(
     test_context: AppContext, client: FlaskClient, seed_person: Person
 ):
     with test_context:
-        response = client.get(f"/api/persons/{seed_person.id}/segment")
+        response = client.get(f"/api/persons/{seed_person.id}/segments?limit=true")
         assert response.status_code == 404
         assert response.json == {
-            "error": "person does not have an segment, please create one"
+            "error": "person does not have a segment, please create one"
         }
+
+
+def test_add_segment(
+    test_context: AppContext,
+    client: FlaskClient,
+    seed_person: Person,
+    segment_data: dict,
+):
+    with test_context:
+        response = client.put(
+            f"/api/persons/{seed_person.id}/segments?limit=true",
+            json=segment_data,
+        )
+    assert response.status_code == 200
+    assert response.json["start_date"] == "2021-01-01"
+    assert response.json["city"] == "San Francisco"
+    assert response.json["state"] == "CA"
+    assert response.json["person_id"] == str(seed_person.id)
+
+
+def test_add_segment_user_not_found(
+    test_context: AppContext, client: FlaskClient, segment_data: dict
+):
+    with test_context:
+        response = client.put(
+            f"/api/persons/{uuid.uuid4()}/segments?limit=true",
+            json=segment_data,
+        )
+        assert response.status_code == 404
+        assert response.json == {"error": "No person found for segment"}
+
+
+def test_add_two_segments(
+    test_context: AppContext, client: FlaskClient, seed_person: Person
+):
+    # this simply tests that we can add two segments for each user,
+    # as long as their start dates and end dates arte correct
+    with test_context:
+        uri = f"/api/persons/{seed_person.id}/segments"
+        response1 = client.put(
+            uri,
+            json={
+                "start_date": "2021-01-01",
+                "end_date": "2021-01-02",
+                "city": "San Francisco",
+                "state": "CA",
+                "zip_code": "94613",
+            },
+        )
+        response2 = client.put(
+            uri,
+            json={
+                "start_date": "2021-01-03",
+                "city": "Los Angeles",
+                "state": "CA",
+                "zip_code": "94444",
+            },
+        )
+        all_segments = client.get(f"{uri}?limit=false")
+
+    assert response1.status_code == 200
+    assert response1.json["city"] == "San Francisco"
+    assert response1.json["start_date"] == "2021-01-01"
+
+    assert response2.status_code == 200
+    assert response2.json["city"] == "Los Angeles"
+    assert response2.json["start_date"] == "2021-01-03"
+
+    assert len(all_segments.json) > 1
+
+
+def test_update_segment_existing(
+    test_context: AppContext, client: FlaskClient, seed_person: Person
+):
+    with test_context:
+        uri = f"/api/persons/{seed_person.id}/segments"
+        segment_data = {
+            "start_date": "2021-01-01",
+            "end_date": "2021-01-02",
+            "city": "San Francisco",
+            "state": "CA",
+            "zip_code": "94613",
+        }
+        segment_call_1 = client.put(
+            uri,
+            json=segment_data,
+        )
+        segment_call_2 = client.put(
+            uri,
+            json=segment_data,
+        )
+    for key, value in segment_call_1.json.items():
+        if not key == "id":
+            assert value == segment_call_2.json[key]
+
+
+def test_update_segment_no_end_date(
+    test_context: AppContext, client: FlaskClient, seed_person: Person
+):
+    with test_context:
+        uri = f"/api/persons/{seed_person.id}/segments"
+        segment = client.put(
+            uri,
+            json={
+                "start_date": "2021-01-01",
+                "city": "San Francisco",
+                "state": "CA",
+                "zip_code": "94613",
+            },
+        )
+    assert segment.json["end_date"] is None
+
+
+def test_add_two_segments_updating_end_date(
+    test_context: AppContext, client: FlaskClient, seed_person: Person
+):
+    with test_context:
+        uri = f"/api/persons/{seed_person.id}/segments"
+        segment_1 = client.put(
+            uri,
+            json={
+                "start_date": "2021-01-01",
+                "city": "San Francisco",
+                "state": "CA",
+                "zip_code": "94613",
+            },
+        )
+        segment_2 = client.put(
+            uri,
+            json={
+                "start_date": "2021-01-02",
+                "end_date": "2021-01-03",
+                "city": "Los Angeles",
+                "state": "CA",
+                "zip_code": "94518",
+            },
+        )
+        all_segments = client.get(f"{uri}?limit=false")
+    for segment in all_segments.json:
+        if segment["zip_code"] == "94613":
+            assert segment["end_date"] == "2021-01-02"
+
+
+def test_update_segment_datetime_same_dates(
+    test_context: AppContext, client: FlaskClient, seed_person: Person
+):
+    uri = f"/api/persons/{seed_person.id}/segments"
+    with test_context:
+        original_segment = client.put(
+            uri,
+            json={
+                "start_date": "2021-01-01",
+                "end_date": "2021-01-02",
+                "city": "San Francisco",
+                "state": "CA",
+                "zip_code": "94613",
+            },
+        )
+        response = client.put(
+            uri,
+            json={
+                "start_date": "2021-01-01",
+                "city": "Los Angeles",
+                "state": "CA",
+                "zip_code": "94518",
+            },
+        )
+    assert response.status_code == 200
+    assert response.json["id"] == original_segment.json["id"]
+
+
+def test_update_segment_datetime_mistmatch(
+    test_context: AppContext, client: FlaskClient, seed_person: Person
+):
+    uri = f"/api/persons/{seed_person.id}/segments"
+    with test_context:
+        original_segment = client.put(
+            uri,
+            json={
+                "start_date": "2021-01-01",
+                "end_date": "2021-01-03",
+                "city": "San Francisco",
+                "state": "CA",
+                "zip_code": "94613",
+            },
+        )
+        response = client.put(
+            uri,
+            json={
+                "start_date": "2021-01-02",
+                "city": "Los Angeles",
+                "state": "CA",
+                "zip_code": "94518",
+            },
+        )
+    assert response.status_code == 422
